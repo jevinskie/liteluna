ecf9920996fc435e29964b350e1dda96ace046f8
Jevin Sweval <jevinsweval@gmail.com>
2022-05-30 11:48:45 -0700

    wip

--- examples/sim_loopback_server.py
+++ examples/sim_loopback_server.py
@@ -5,6 +5,8 @@ import time
 
 from usbip_toolkit.usb import *
 
+DEV_ADDR = 0x2B
+
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
@@ -12,6 +14,15 @@ serv_sock.bind(("localhost", 2443))
 serv_sock.listen(1)
 
 s, _ = serv_sock.accept()
+
+
+def get_odd(reset=False):
+    get_odd.odd = getattr(get_odd, "odd", False)
+    if reset:
+        get_odd.odd = False
+    odd = get_odd.odd
+    get_odd.odd = not get_odd.odd
+    return odd
 
 
 def read():
@@ -38,38 +49,38 @@ def write(bufs):
 
 input("Press ANY key\n")
 
-odd = False
-
 while True:
     sof1_packet = sof_packet(0)
     print(f"sof1_packet: {sof1_packet.hex()}")
     sof2_packet = sof_packet(1)
     print(f"sof2_packet: {sof2_packet.hex()}")
 
-    setup_token_packet = setup_packet(0, 0)
-    print(f"setup_token_packet: {setup_token_packet.hex()}")
+    setup_token = setup_token_packet(0, 0)
+    print(f"setup_token: {setup_token.hex()}")
 
     # setup_buf = bytes.fromhex("8006000100004000")
     setup_buf = bytes.fromhex("00052B0000000000")
-    setup_data_packet, odd = data_packet(setup_buf, odd=odd)
+    setup_data_packet = data_packet(setup_buf, odd=False)
     print(f"setup_data_packet: {setup_data_packet.hex()}")
 
-    write([sof1_packet, sof2_packet, setup_token_packet, setup_data_packet])
+    write([sof1_packet, sof2_packet, setup_token, setup_data_packet])
 
     in_buf = read()
     print(f"in_buf: {in_buf.hex()}")
 
     sof3_packet = sof_packet(2)
 
-    in_token_packet = in_packet(0, 0)
-    print(f"in_token_packet: {in_token_packet.hex()}")
-    # write([sof3_packet, in_token_packet])
-    write(in_token_packet)
+    in_token = in_token_packet(0, 0)
+    print(f"in_token: {in_token.hex()}")
+    write([sof3_packet, in_token])
+    # write(in_token)
 
     in_buf = read()
     print(f"in_buf: {in_buf.hex()}")
 
-    in_buf = read()
-    print(f"in_buf: {in_buf.hex()}")
+    write(ack_packet())
+
+    # in_buf = read()
+    # print(f"in_buf: {in_buf.hex()}")
 
     input("Press ANY key\n")
