3295e8b2e7cb05048fdc122c23598998d185a6a7
Jevin Sweval <jevinsweval@gmail.com>
2022-06-07 15:28:28 -0700

    replay improvement

--- examples/sim_server_replay.py
+++ examples/sim_server_replay.py
@@ -12,13 +12,14 @@ serv_sock.bind(("localhost", 2443))
 serv_sock.listen(1)
 
 s, _ = serv_sock.accept()
+s.settimeout(1)
 
 
 def read():
     sz_buf = s.recv(4)
     sz = int.from_bytes(sz_buf, "big")
     buf = s.recv(sz)
-    print(f"d2h_raw: {buf.hex(' ')}", flush=True)
+    # print(f"d2h_raw: {buf.hex(' ')}", flush=True)
     return buf
 
 
@@ -28,21 +29,35 @@ def write(bufs):
     out_buf = b""
     for buf in bufs:
         out_buf += len(buf).to_bytes(4, "big") + buf
-        print(f"h2d_raw: {buf.hex(' ')}", flush=True)
+        # print(f"h2d_raw: {buf.hex(' ')}", flush=True)
     s.send(out_buf)
 
 
 for rl in replay_lines:
-    if rl.startswith("h2d_raw: "):
+    if rl.startswith("#"):
+        print(rl)
+    if rl.startswith("h2d_raw: ") or rl.startswith("h2d: "):
         hs = rl.removeprefix("h2d_raw: ")
+        hs = hs.removeprefix("h2d: ")
         obuf = bytes.fromhex(hs)
         print(f"h2d: {obuf.hex(' ')}")
         write(obuf)
-    if rl.startswith("d2h_raw: "):
+    if rl.startswith("d2h_raw: ") or rl.startswith("d2h: "):
         hs = rl.removeprefix("d2h_raw: ")
+        hs = hs.removeprefix("d2h: ")
         ibuf_gold = bytes.fromhex(hs)
         print(f"ibuf expected: {ibuf_gold.hex(' ')}")
         ibuf = read()
         print(f"ibuf actual:   {ibuf.hex(' ')}")
         if ibuf != ibuf_gold:
             print("MISMATCH")
+
+i = 0
+while True:
+    try:
+        ibuf = read()
+        print(f"extra ibuf {i}: {ibuf.hex(' ')}")
+        i += 1
+    except TimeoutError:
+        print("no more leftovers")
+        break
